22873653e5fd772887e309184dfe0011a0fac575
Kacper Grzymkowski <kacper.grzymkowski00@gmail.com>
2023-12-09 15:44:39 +0100

    Create create-topics.sh

---
+++ create-topics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "nifi2vit"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "nifi2ocr"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "nifi2analytics"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "vit2cluster"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "vit2analytics"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "cluster2analytics"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "ocr2ner"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "ocr2sentiment"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "ocr2analytics"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "ner2analytics"
+/home/nifi/kafka_2.13-3.6.0/bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic "sentiment2analytics"
