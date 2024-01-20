1357a9bd5a26680bb382ff76358e2145ba7f9529
Kacper Grzymkowski <kacper.grzymkowski00@gmail.com>
2024-01-20 18:31:55 +0100

    Update clusterjob.py

--- processing/clusterjob.py
+++ processing/clusterjob.py
@@ -9,7 +9,7 @@ from sklearn.cluster import Birch
 
 spark = SparkSession.builder.appName('cluster').getOrCreate()
 
-model = Birch()
+model = Birch(n_clusters=100)
 n_items = [0]
 
 # scikit complains about clusters
